ddc34a8f86a54962f4d0a7326ba0c38104290cff
Takanori Ugai <ugai.takanori@gmail.com>
2026-03-13 10:45:16 +0900

    Update build.gradle.kts

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j-open-ai:1.12.2")
     implementation("dev.langchain4j:langchain4j-azure-open-ai:1.12.2")
     implementation("dev.langchain4j:langchain4j-ollama:1.12.2")
-    implementation("dev.langchain4j:langchain4j-community-neo4j:1.12.2-beta19")
+    implementation("dev.langchain4j:langchain4j-community-neo4j:1.12.2-beta21")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.10")
     testImplementation("io.mockk:mockk:1.14.9")
