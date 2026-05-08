91de9fae29062f0f48d648e4064e988baa76f9b3
Takanori Ugai <ugai.takanori@gmail.com>
2026-05-08 14:38:13 +0900

    Update langchain4j-community-neo4j version to beta24

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j-open-ai:1.14.1")
     implementation("dev.langchain4j:langchain4j-azure-open-ai:1.14.1")
     implementation("dev.langchain4j:langchain4j-ollama:1.14.1")
-    implementation("dev.langchain4j:langchain4j-community-neo4j:1.14.1-beta23")
+    implementation("dev.langchain4j:langchain4j-community-neo4j:1.14.1-beta24")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.21")
     testImplementation("io.mockk:mockk:1.14.9")
